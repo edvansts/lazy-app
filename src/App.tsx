df1d61d211c4285400df5d71fa7faee76cd97f47
Edvan de Matos <edvan.stt02@gmail.com>
2023-02-15 12:27:54 -0300

    feat: :sparkles: lazy loading count button

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { lazy, Suspense, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+const Count = lazy(() =>
+  import("./components/count").then((module) => ({ default: module.Count }))
+);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isShowingCount, setIsShowingCount] = useState(false);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <Suspense fallback="loading...">
+      <div className="App">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          {isShowingCount && <Count />}
+
+          <div>
+            <button onClick={() => setIsShowingCount((value) => !value)}>
+              {isShowingCount ? "Hide" : "Show"} Count Button
+            </button>
+          </div>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/count/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function Count() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <button onClick={() => setCount((count) => count + 1)}>
+      count is {count}
+    </button>
+  );
+}
+
+export { Count };
